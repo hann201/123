9773f1f6e8afb66565a94a807bdeaf3afa5ac94c
Hanna <>
2023-12-07 21:31:10 +0800

    12/7

---
+++ asset/js/countdown.js
@@ -0,0 +1,11 @@
+const initEnd = () => {
+    let end = new Date;
+    end.setFullYear(2024);
+    end.setMonth(0);
+    end.setDate(1);
+    end.setHours(0);
+    end.setMinutes(0);
+    end.setSeconds(0);
+    return end;
+}
+
