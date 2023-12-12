79298e03bc5a3a2dec43e985d875d006ae4802f7
Hanna <>
2023-12-12 21:29:04 +0800

    12/12

--- asset/js/todolist_demo.js
+++ asset/js/todolist_demo.js
@@ -1,39 +1,21 @@
 let elInput = document.querySelector('#todo-in');
 let elAddBtn = document.querySelector('#todo-add-btn');
 let elItem = document.querySelector('#todo-item');
-let items = [];
-
-// item = {checked: false, text: ''}
-
-/**
- * 將 items 資料使用迴圈組成 HTML 並顯示
- */
-const render = () => {
-    let html = '';
-    items.forEach((item, index) => {
-        let checked = item.checked ? 'checked' : '';
-
-        html += `<li data-index="${index}">
-                    <input type="checkbox" ${checked}>
-                    <span>${item.text}</span>
-                </li>`
-    })
-
-    elItem.innerHTML = html;
-}
 
 const addTodo = () => {
     let value = elInput.value;
     if (!value) {
         elInput.focus();
-        return; // void
+        return;
     }
+
+    elItem.innerHTML += `<li>
+                            <input type="checkbox">
+                            <span>${value}</span>
+                        </li>`
 
     elInput.value = '';
     elInput.focus();
-
-    items.push({ checked: false, text: value });
-    render();
 }
 
 elAddBtn.addEventListener('click', (e) => {
@@ -50,14 +32,20 @@ elInput.addEventListener('keyup', (e) => {
 elItem.addEventListener('click', (e) => {
     let el = e.target;
     let tag = el.tagName.toString().toUpperCase();
+    let isCheckbox = false;
+
+    if (tag == 'INPUT') {
+        isCheckbox = true;
+    }
 
     if (tag == 'SPAN' || tag == 'INPUT') {
-        el = el.parentNode;
+        el = el.parentNode
     }
 
     if (el.tagName.toString().toUpperCase() == 'LI') {
-        let index = el.dataset.index;
-        items[index].checked = !items[index].checked;
-        render();
+        let checkbox = el.querySelector('input[type=checkbox]');
+        if (!isCheckbox) {
+            checkbox.checked = !checkbox.checked;
+        }
     }
 })
