42a2215812613ae776ba948d3ef41b504160d517
Hanna <>
2023-12-21 20:02:12 +0800

    todolist realtime

--- asset/js/firebase_realtime.js
+++ asset/js/firebase_realtime.js
@@ -5,3 +5,15 @@ const app = await App.init()
 const database = new Database(app)
 
 // let response = await database.write("a", { a: 1, b: 2, c: 3, d: 4 });
+
+// let response = await database.read('david');
+// console.log(response);
+
+const onChange = (data) => {
+    console.log(data);
+}
+
+database.listen('david', onChange);
+
+
+database.insert('test', 'aaaa');
